ffcd36ca8ef073b9a405dba89f7bd9d47b0fee8d
RaviRajBhardwaj <ravirajbhardwaaj@gmail.com>
2025-01-07 17:34:46 +0530

    fix(bug): minnor bugs

--- calculator.js
+++ calculator.js
@@ -4,20 +4,20 @@ class Calculator {
         this.numTwo = numTwo;
     }
     add() {
-        console.log(`${this.numOne} + ${this.numTwo}`);
+        console.log(`${this.numOne} + ${this.numTwo} =`, this.numOne + this.numTwo);
     }
 
     subtract() {
-        console.log(`${this.numOne} - ${this.numTwo}`);
+        console.log(`${this.numOne} - ${this.numTwo} =`, this.numOne - this.numTwo);
     }
 
     multiply() {
-        console.log(`${this.numOne} * ${this.numTwo}`);
+        console.log(`${this.numOne} * ${this.numTwo} =`, this.numOne * this.numTwo);
     }
 
     divide() {
         if (this.numTwo !== 0) {
-            console.log(`${this.numOne} / ${this.numTwo}`);
+            console.log(`${this.numOne} / ${this.numTwo} =`, this.numOne / this.numTwo);
         } else {
             console.log('Error: Division by zero');
         }
